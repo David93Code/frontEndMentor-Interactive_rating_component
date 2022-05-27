cae91093816e52b19bfb4422aa1283b338148566
David93Code <98108485+David93Code@users.noreply.github.com>
2022-05-27 12:12:37 +0300

    Add files via upload

---
+++ Resources/app.js
@@ -0,0 +1,13 @@
+
+
+function showDiv() {
+    document.getElementById('hidden').style.visibility = "visible";
+
+}
+
+
+function showNum(element) {
+    document.getElementById("parag-for-message").textContent = "You selected " + element.value + " out of 5.";
+
+}
+
